c096ed9544fcdcb362d656f6daf53c40e7cc89fd
Awe42 <anish.pathak5@gmail.com>
2022-11-20 10:51:23 +0100

    Test

--- src/pages/Trade/index.js
+++ src/pages/Trade/index.js
@@ -65,7 +65,6 @@ const Trade = () => {
 	const submitOrder = () => {
 		axios.post(`https://bloomberg-backend.herokuapp.com/${type.toLowerCase()}`, {type: type, side: side, security: security, price: price, qty: qty, user: user})
 			.then((response) => console.log(response.status))
-			.then(window.location.reload(false))
 			.catch((error) => console.error(error));
 	}
 	
